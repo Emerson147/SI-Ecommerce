597155ca5d5947182ebc7ee3ef3d5d446b8797a7
Emerson <emersontec147@gmail.com>
2023-06-05 15:16:04 -0500

    Segundo commit - agregando api de whatsapp

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+import { AiOutlineWhatsApp } from 'react-icons/ai';
+
 const Layout = ({ children }) => {
   return (
     <div className="layout">
@@ -15,6 +17,16 @@ const Layout = ({ children }) => {
       </header>
       <main className="main-container">
         {children}
+      <a href="https://api.whatsapp.com/send?phone=51926585479&text=Hola, me gustaria Obtener más información acerca de los productos..."
+      className="float pulse"
+      target="_blank"
+      >
+      <p className="my-float pulse">
+        <AiOutlineWhatsApp />
+      </p>
+
+      </a>
+      
       </main>
       <footer>
         <Footer />
