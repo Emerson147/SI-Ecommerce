6081c93c3cf69f29b5f44acf9e35ff6e9855b436
Emerson Rafael <56899061+Emerson147@users.noreply.github.com>
2023-06-09 14:29:55 -0500

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -99,7 +99,7 @@ const Cart = () => {
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
-                Paga con Sripe
+                Paga con Stripe
               </button>
             </div>
           </div>
