d41be4b0a9a03797d3f52c570a87300e89c9c700
Emerson Rafael <56899061+Emerson147@users.noreply.github.com>
2023-06-09 13:45:05 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">AIR BACKSTAGE</Link>
+        <Link href="/">IMPORTACIONES DEN</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
